0ab58ba256f5fef1c63743bede4c2fa4d96f4ede
naol86 <naol.kasinet@a2sv.org>
2024-03-27 09:39:40 +0300

    pizza component

--- 03-pizza-menu/pizzaman/src/App.js
+++ 03-pizza-menu/pizzaman/src/App.js
@@ -1,25 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
+
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
 
 function App() {
-    const [advice, setAdvice] = useState("");
-
-    const getAdvice = async () => {
-        const response = await fetch("https://api.adviceslip.com/advice");
-        const data = await response.json();
-        setAdvice(data.slip.advice);
-    };
-
     return (
         <div>
-            <h1>hello world 2</h1>
-            <button onClick={getAdvice}>new advice</button>
-            <DisplayMessage message={advice} />
+            <h1>hello world</h1>
+            <Pizza />
+            <Pizza />
         </div>
     );
 }
 
-function DisplayMessage(props) {
-    return <div>{props.message}</div>;
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/focaccia.jpg" alt="Focaccia Pizza" />
+            <h2>Focaccia Pizza</h2>
+            <p>Bread with italian olive oil and rosemary</p>
+        </div>
+    );
 }
 
 export default App;
