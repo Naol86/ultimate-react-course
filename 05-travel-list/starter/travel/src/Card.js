ff3490881e5c1f182ed66291b669e8f94bd43b3a
naol86 <naol.kasinet@a2sv.org>
2024-04-04 12:26:51 +0300

    card test

--- 05-travel-list/starter/travel/src/Card.js
+++ 05-travel-list/starter/travel/src/Card.js
@@ -1,7 +1,51 @@
-const card = document.querySelector(".card");
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
 
-card.addEventListener("click", () => {
-  console.log("clicked");
-  card.classList.toggle("answer");
-  card.textContent = "answer";
-});
+export default function Card() {
+  return (
+    <div>
+      <Questions />
+    </div>
+  );
+}
+
+function Questions() {
+  return (
+    <div>
+      {questions.map((question) => (
+        <div key={question.id}>{question.question}</div>
+      ))}
+    </div>
+  );
+}
--- 05-travel-list/starter/travel/src/index.js
+++ 05-travel-list/starter/travel/src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import Card from "./Card";
 import React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Card />
   </React.StrictMode>
 );
